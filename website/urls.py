d175824e6cb881e7998165e1e4fa20d49a33fe74
Jordan Miracle <jordanmiracle@protonmail.com>
2021-03-09 04:20:44 -0800

    Added images, and have made this PNW2. A bit of a turn, but seing as it is not much different from PNW, I think it will work out much better for the company.

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+
+
 urlpatterns = [
     path('', views.index, name="index"),
+    path('generic.html', views.generic, name='generic'),
+    path('elements.html', views.elements, name='elements'),
+
 ]
--- website/views.py
+++ website/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html', {})
 
+
+def generic(request):
+    return render(request, 'generic.html', {})
+
+
+def elements(request):
+    return render(request, 'elements.html', {})
+
 # Create your views here.
