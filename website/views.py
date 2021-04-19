ed2991836da878275293211ee48f09a39eea2c83
Jordan Miracle <jordanmiracle@protonmail.com>
2021-04-18 17:42:57 -0700

    Changed the subtitle to areas served. Got rid of empty dictionaries in views. Need to add links to meu items.

--- website/views.py
+++ website/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, 'index.html')
 
 
 def generic(request):
-    return render(request, 'generic.html', {})
+    return render(request, 'generic.html')
 
 
 def elements(request):
-    return render(request, 'elements.html', {})
+    return render(request, 'elements.html')
 
 # Create your views here.
