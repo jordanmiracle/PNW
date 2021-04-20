c0a446578edbfdd2f8e0aab97ceb506d4ec54c28
Jordan Miracle <jordanmiracle@protonmail.com>
2021-04-20 04:14:55 -0700

    Refined robots.txt. It's working now.

--- website/urls.py
+++ website/urls.py
@@ -9,6 +9,8 @@ from django.contrib.sitemaps import GenericSitemap # new
 from django.contrib.sitemaps.views import sitemap
 from .sitemaps import Static_Sitemap, Review_Sitemap
 from website.models import Reviews
+from django.views.generic.base import TemplateView  # import TemplateView
+
 
 sitemaps = {
     'article': Review_Sitemap(),
@@ -24,5 +26,6 @@ urlpatterns = [
     path('about.html', views.generic, name='generic'),
     path('elements.html', views.elements, name='elements'),
     path('sitemap.xml', sitemap, {'sitemaps':{'Reviews':GenericSitemap(info_dict, priority=0.6)}}, name='django.contrib.sitemaps.views.sitemap'),
+    path("robots.txt", TemplateView.as_view(template_name="website/robots.txt", content_type="text/plain")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
