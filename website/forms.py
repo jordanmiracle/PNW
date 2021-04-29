7217c7edbd6411a1580ae165296983652c1b3302
Jordan Miracle <jordanmiracle@protonmail.com>
2021-04-29 03:18:09 -0700

    built out the initial form for the comment and ratings.

--- website/forms.py
+++ website/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import *
+from django.forms import ModelForm, Textarea
+from .models import Review
+
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ()
+        fields = ('rating', 'comment')
+        wisgets = {
+            'comment': Textarea(attrs={'cols': 40, 'rows': 15})
+        }
