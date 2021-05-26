a26b516e78a16bcff72aeae867d3f1492d00575b
Jordan Miracle <jordanmiracle@protonmail.com>
2021-05-26 04:23:23 -0700

    got rid of extra bootstrap and am still trying to figure out the reviews. Also, made a change to the models that allows for blank descriptions.

--- photos/models.py
+++ photos/models.py
@@ -11,7 +11,7 @@ class Category(models.Model):
 class Photo(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(null=False, blank=False)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.description
