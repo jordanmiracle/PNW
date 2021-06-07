a099a052e6a8c439b02cc72633a64739078f1ae1
Jordan Miracle <jordanmiracle@protonmail.com>
2021-06-07 01:55:58 -0700

    Added django compressor and updated requirements.txt.

--- PNWproject/settings.py
+++ PNWproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
     'photos.apps.PhotosConfig',
+    'compressor',
 
 ]
 
@@ -125,6 +126,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 
 ]
+
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 MEDIA_URL = '/media/images/'
