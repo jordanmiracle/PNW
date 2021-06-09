402f9b4f5638149974dc2455adf01042973358fa
Jordan Miracle <jordanmiracle@protonmail.com>
2021-06-08 19:43:16 -0700

    changed static settings for aws

--- PNWproject/settings.py
+++ PNWproject/settings.py
@@ -153,7 +153,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static')
 ]
@@ -166,7 +165,7 @@ MEDIA_URL = '/images/'
 #]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-#STATIC_TMP = BASE_DIR / 'static'
+STATIC_TMP = BASE_DIR / 'static'
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
@@ -178,8 +177,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 AWS_ACCESS_KEY_ID = 'AKIAXCHWTHMXUSTYOJPM'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'django-personal-s3'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_LOCATION = 'static'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
 AWS_S3_FILE_OVERWRITE = False
-#AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazomaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
