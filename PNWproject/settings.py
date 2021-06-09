59aac4883d43336f1a0ffb8744b479946dc67946
Jordan Miracle <jordanmiracle@protonmail.com>
2021-06-08 17:49:24 -0700

    removed staticfiles_dirs

--- PNWproject/settings.py
+++ PNWproject/settings.py
@@ -147,9 +147,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static')
+#]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 MEDIA_URL = '/media/images/'
