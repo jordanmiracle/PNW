8d9b94846d15e3a0c2052bf596a5094912127094
Jordan Miracle <jordanmiracle@protonmail.com>
2021-06-14 01:05:46 -0700

    The biggest was putting the aws keys straight into the settings file instead of using secrets.json. Changed from ImageField to FileField, and removed all images tags that started with slashes.

--- photos/models.py
+++ photos/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
 
 class Photo(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
-    image = models.ImageField(null=False, blank=False)
+    image = models.FileField(null=False, blank=False)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
